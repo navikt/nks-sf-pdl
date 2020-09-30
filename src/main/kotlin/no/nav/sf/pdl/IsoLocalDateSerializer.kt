dd9dbb0632c9bf96ce241411d1aa581cf5fcc87f
ADEO\S160163 <marius.skalstad@nav.no>
2020-09-30 09:53:46 +0200

    Removed wildcard import from IsoLocalDateSerializer.kt

--- src/main/kotlin/no/nav/sf/pdl/IsoLocalDateSerializer.kt
+++ src/main/kotlin/no/nav/sf/pdl/IsoLocalDateSerializer.kt
@@ -1,9 +1,13 @@
 package no.nav.sf.pdl
 
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import kotlinx.serialization.*
+import kotlinx.serialization.Decoder
+import kotlinx.serialization.Encoder
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.PrimitiveDescriptor
+import kotlinx.serialization.PrimitiveKind
+import kotlinx.serialization.SerialDescriptor
 
 object IsoLocalDateSerializer : LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE)
 open class LocalDateSerializer(private val formatter: DateTimeFormatter) : KSerializer<LocalDate> {
@@ -12,16 +16,6 @@ open class LocalDateSerializer(private val formatter: DateTimeFormatter) : KSeri
         return LocalDate.parse(decoder.decodeString(), formatter)
     }
     override fun serialize(encoder: Encoder, value: LocalDate) {
-        encoder.encodeString(value.format(formatter))
-    }
-}
-object IsoLocalDateTimeSerializer : LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
-open class LocalDateTimeSerializer(private val formatter: DateTimeFormatter) : KSerializer<LocalDateTime> {
-    override val descriptor: SerialDescriptor = PrimitiveDescriptor("LocalDateTime", PrimitiveKind.STRING) // StringDescriptor.withName(“java.time.LocalDateTime”)
-    override fun deserialize(decoder: Decoder): LocalDateTime {
-        return LocalDateTime.parse(decoder.decodeString(), formatter)
-    }
-    override fun serialize(encoder: Encoder, value: LocalDateTime) {
         encoder.encodeString(value.format(formatter))
     }
 }
