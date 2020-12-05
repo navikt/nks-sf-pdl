48cce11fa13f90068e8cb31c620eff5035b73bc2
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2020-12-05 14:59:25 +0100

    Missing env var for prod

--- src/main/kotlin/no/nav/sf/pdl/Bootstrap.kt
+++ src/main/kotlin/no/nav/sf/pdl/Bootstrap.kt
@@ -30,7 +30,7 @@ object Bootstrap {
             log.info { "Starting - grace period 3 m after enableNAISAPI" }
             conditionalWait(180000)
             log.info { "Starting - post grace period enableNAISAPI" }
-            // gtInitLoad(ws)
+            gtInitLoad(ws)
             // initLoadTest(ws)
             // initLoad(ws)
             loadPersonCache(ws) // Will carry cache in memory after this point
