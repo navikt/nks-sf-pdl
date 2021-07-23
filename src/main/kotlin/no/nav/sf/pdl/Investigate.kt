f42588471b6509f9e3ff8d9adb2bd7d5298f7d63
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2021-07-23 15:40:06 +0200

    Mute Investigate trace and turn off init investigate

--- src/main/kotlin/no/nav/sf/pdl/Investigate.kt
+++ src/main/kotlin/no/nav/sf/pdl/Investigate.kt
@@ -1,6 +1,5 @@
 package no.nav.sf.pdl
 
-import java.io.FileOutputStream
 import java.time.Instant
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
@@ -12,9 +11,9 @@ interface Investigate {
                     .ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
                     .withZone(ZoneOffset.systemDefault())
                     .format(Instant.now())
-            FileOutputStream(filename, append).bufferedWriter().use { writer ->
-                writer.write("$timeStamp : $text \n")
-            }
+            // FileOutputStream(filename, append).bufferedWriter().use { writer ->
+            //    writer.write("$timeStamp : $text \n")
+            // } //TODO Muted Investigate for later use
         }
     }
 }
