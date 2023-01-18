4a360a1e044b55f0fabed403c96ed0f559d4883c
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2023-01-18 12:34:39 +0100

    Dev try avro

--- src/main/kotlin/no/nav/sf/pdl/Migration.kt
+++ src/main/kotlin/no/nav/sf/pdl/Migration.kt
@@ -1,20 +1,23 @@
 package no.nav.sf.pdl
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import java.io.File
 import mu.KotlinLogging
 import no.nav.sf.library.AKafkaConsumer
 import no.nav.sf.library.KafkaConsumerStates
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.config.SslConfigs
-import org.apache.kafka.common.serialization.ByteArrayDeserializer
 
 private val log = KotlinLogging.logger {}
 
 fun checkLatestFeed() {
     var retries = 5
     val kafkaConsumerGcpMigration: Map<String, Any> = AKafkaConsumer.configBase + mapOf<String, Any>(
-        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java,
-        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java,
+        // ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java,
+        // ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java,
+        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
+        "schema.registry.url" to kafkaSchemaReg,
         "security.protocol" to "SSL",
         SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG to fetchEnv(EV_kafkaKeystorePath),
         SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG to fetchEnv(EV_kafkaCredstorePassword),
@@ -31,61 +34,76 @@ fun checkLatestFeed() {
         return
     }
 
-    AKafkaConsumer<String, String?>(
-        config = kafkaConsumerGcpMigration,
-        topics = listOf("pdl.pdl-persondokument-v1"),
-        fromBeginning = true
-    ).consume { cRecords ->
-        log.info { "Inside Migration Consumer" }
-        if (cRecords.isEmpty) {
-            if (workMetrics.recordsParsed.get().toInt() == 0 && retries > 0) {
-                log.info { "Migration: No records found $retries retries left, wait 60 w" }
-                retries--
-                Bootstrap.conditionalWait(60000)
-                return@consume KafkaConsumerStates.IsOk
-            } else {
-                log.info { "Migration: No more records found (or given up) - end consume session" }
-                return@consume KafkaConsumerStates.IsFinished
+    try {
+        AKafkaConsumer<String, String?>(
+            config = kafkaConsumerGcpMigration,
+            topics = listOf("pdl.pdl-persondokument-v1"),
+            fromBeginning = true
+        ).consume { cRecords ->
+            log.info { "Inside Migration Consumer" }
+            if (cRecords.isEmpty) {
+                if (workMetrics.recordsParsed.get().toInt() == 0 && retries > 0) {
+                    log.info { "Migration: No records found $retries retries left, wait 60 w" }
+                    retries--
+                    Bootstrap.conditionalWait(60000)
+                    return@consume KafkaConsumerStates.IsOk
+                } else {
+                    log.info { "Migration: No more records found (or given up) - end consume session" }
+                    return@consume KafkaConsumerStates.IsFinished
+                }
             }
-        }
-        workMetrics.recordsParsed.inc(cRecords.count().toDouble())
+            workMetrics.recordsParsed.inc(cRecords.count().toDouble())
 
-        val results = cRecords.map { cr ->
-            if (cr.value() == null) {
-                val personTombestone = PersonTombestone(aktoerId = cr.key())
-                // workMetrics.tombstones.inc()
-                // Investigate.writeText("CONSUMED PERSON OFFSET ${cr.offset()} TOMBSTONE", true)
-                Triple(KafkaConsumerStates.IsOk, personTombestone, cr.offset())
-            } else {
-                when (val query = cr.value()!!.getQueryFromJson()) {
-                    InvalidQuery -> {
-                        workMetrics.consumerIssues.inc()
-                        log.error { "Unable to parse topic value PDL" }
-                        Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
-                    }
-                    is Query -> {
-                        when (val personSf = query.toPersonSf()) {
-                            is PersonSf -> {
-                                Triple(KafkaConsumerStates.IsOk, personSf, cr.offset())
-                            }
-                            is PersonInvalid -> {
-                                workMetrics.consumerIssues.inc()
-                                Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
-                            }
-                            else -> {
-                                workMetrics.consumerIssues.inc()
-                                log.error { "Returned unhandled PersonBase from Query.toPersonSf" }
-                                Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
+            val results = cRecords.map { cr ->
+                if (cr.value() == null) {
+                    val personTombestone = PersonTombestone(aktoerId = cr.key())
+                    // workMetrics.tombstones.inc()
+                    // Investigate.writeText("CONSUMED PERSON OFFSET ${cr.offset()} TOMBSTONE", true)
+                    Triple(KafkaConsumerStates.IsOk, personTombestone, cr.offset())
+                } else {
+                    when (val query = cr.value()!!.getQueryFromJson()) {
+                        InvalidQuery -> {
+                            workMetrics.consumerIssues.inc()
+                            log.error { "Unable to parse topic value PDL" }
+                            Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
+                        }
+
+                        is Query -> {
+                            when (val personSf = query.toPersonSf()) {
+                                is PersonSf -> {
+                                    Triple(KafkaConsumerStates.IsOk, personSf, cr.offset())
+                                }
+
+                                is PersonInvalid -> {
+                                    workMetrics.consumerIssues.inc()
+                                    Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
+                                }
+
+                                else -> {
+                                    workMetrics.consumerIssues.inc()
+                                    log.error { "Returned unhandled PersonBase from Query.toPersonSf" }
+                                    Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
+                                }
                             }
                         }
                     }
                 }
             }
+            val areOk = results.map { it.first }.filterIsInstance<KafkaConsumerStates.HasIssues>().isEmpty()
+            log.info {
+                "Migration check finished with ok flag $areOk count ${results.size} offsets ${
+                    cRecords.first().offset()
+                } to ${cRecords.last().offset()}"
+            }
+            File("/tmp/migrationcheck").writeText(
+                "ok $areOk count ${results.size} offsets ${
+                    cRecords.first().offset()
+                } to ${cRecords.last().offset()}\nKeys processed:\n${cRecords.map { it.key() }.joinToString("\n")}"
+            )
+            KafkaConsumerStates.IsFinished
         }
-        val areOk = results.map { it.first }.filterIsInstance<KafkaConsumerStates.HasIssues>().isEmpty()
-        log.info { "Migration check finished with ok flag $areOk count ${results.size} offsets ${cRecords.first().offset()} to ${cRecords.last().offset()}" }
-        File("/tmp/migrationcheck").writeText("ok $areOk count ${results.size} offsets ${cRecords.first().offset()} to ${cRecords.last().offset()}\nKeys processed:\n${cRecords.map{it.key()}.joinToString("\n")}")
-        KafkaConsumerStates.IsFinished
+    } catch (e: Exception) {
+        log.error { "Migration consume attempt fail: ${e.message}" }
     }
     log.info { "Migration check exit" }
 }
