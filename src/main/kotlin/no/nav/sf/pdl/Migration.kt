3b7a0c7d46fa47801d60e4930741aae63ed012b1
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2023-01-18 13:53:06 +0100

    Dev Also check GT queue

--- src/main/kotlin/no/nav/sf/pdl/Migration.kt
+++ src/main/kotlin/no/nav/sf/pdl/Migration.kt
@@ -33,80 +33,126 @@ fun checkLatestFeed() {
         log.error { "Abort check latest feed due to config not cascading as expected" }
         return
     }
-
-    try {
-        AKafkaConsumer<String, String?>(
-            config = kafkaConsumerGcpMigration,
-            topics = listOf("pdl.pdl-persondokument-v1"),
-            fromBeginning = true
-        ).consume { cRecords ->
-            log.info { "Inside Migration Consumer" }
-            if (cRecords.isEmpty) {
-                if (workMetrics.recordsParsed.get().toInt() == 0 && retries > 0) {
-                    log.info { "Migration: No records found $retries retries left, wait 60 w" }
-                    retries--
-                    Bootstrap.conditionalWait(60000)
-                    return@consume KafkaConsumerStates.IsOk
-                } else {
-                    log.info { "Migration: No more records found (or given up) - end consume session" }
-                    return@consume KafkaConsumerStates.IsFinished
-                }
+    AKafkaConsumer<String, String?>(
+        config = kafkaConsumerGcpMigration,
+        topics = listOf("pdl.pdl-persondokument-v1"),
+        fromBeginning = true
+    ).consume { cRecords ->
+        log.info { "Inside Migration Consumer" }
+        if (cRecords.isEmpty) {
+            if (workMetrics.recordsParsed.get().toInt() == 0 && retries > 0) {
+                log.info { "Migration: No records found $retries retries left, wait 60 w" }
+                retries--
+                Bootstrap.conditionalWait(60000)
+                return@consume KafkaConsumerStates.IsOk
+            } else {
+                log.info { "Migration: No more records found (or given up) - end consume session" }
+                return@consume KafkaConsumerStates.IsFinished
             }
+        }
 
-            log.info { "Migration consumed batch of ${cRecords.count()}" }
+        log.info { "Migration consumed batch of ${cRecords.count()}" }
 
-            log.info { "Migration first ket ${cRecords.first().key()}" }
+        log.info { "Migration first key ${cRecords.first().key()}" }
 
-            val results = cRecords.map { cr ->
-                if (cr.value() == null) {
-                    val personTombestone = PersonTombestone(aktoerId = cr.key())
-                    // workMetrics.tombstones.inc()
-                    // Investigate.writeText("CONSUMED PERSON OFFSET ${cr.offset()} TOMBSTONE", true)
-                    Triple(KafkaConsumerStates.IsOk, personTombestone, cr.offset())
-                } else {
-                    when (val query = cr.value()!!.getQueryFromJson()) {
-                        InvalidQuery -> {
-                            workMetrics.consumerIssues.inc()
-                            log.error { "Unable to parse topic value PDL" }
-                            Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
-                        }
+        val results = cRecords.map { cr ->
+            if (cr.value() == null) {
+                val personTombestone = PersonTombestone(aktoerId = cr.key())
+                // workMetrics.tombstones.inc()
+                // Investigate.writeText("CONSUMED PERSON OFFSET ${cr.offset()} TOMBSTONE", true)
+                Triple(KafkaConsumerStates.IsOk, personTombestone, cr.offset())
+            } else {
+                when (val query = cr.value()!!.getQueryFromJson()) {
+                    InvalidQuery -> {
+                        workMetrics.consumerIssues.inc()
+                        log.error { "Unable to parse topic value PDL" }
+                        Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
+                    }
 
-                        is Query -> {
-                            when (val personSf = query.toPersonSf()) {
-                                is PersonSf -> {
-                                    Triple(KafkaConsumerStates.IsOk, personSf, cr.offset())
-                                }
+                    is Query -> {
+                        when (val personSf = query.toPersonSf()) {
+                            is PersonSf -> {
+                                Triple(KafkaConsumerStates.IsOk, personSf, cr.offset())
+                            }
 
-                                is PersonInvalid -> {
-                                    workMetrics.consumerIssues.inc()
-                                    Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
-                                }
+                            is PersonInvalid -> {
+                                workMetrics.consumerIssues.inc()
+                                Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
+                            }
 
-                                else -> {
-                                    workMetrics.consumerIssues.inc()
-                                    log.error { "Returned unhandled PersonBase from Query.toPersonSf" }
-                                    Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
-                                }
+                            else -> {
+                                workMetrics.consumerIssues.inc()
+                                log.error { "Returned unhandled PersonBase from Query.toPersonSf" }
+                                Triple(KafkaConsumerStates.HasIssues, PersonInvalid, cr.offset())
                             }
                         }
                     }
                 }
             }
-            val areOk = results.map { it.first }.filterIsInstance<KafkaConsumerStates.HasIssues>().isEmpty()
-            log.info {
-                "Migration check finished with ok flag $areOk count ${results.size} offsets ${
-                    cRecords.first().offset()
-                } to ${cRecords.last().offset()}"
-            }
-            File("/tmp/migrationcheck").writeText(
-                "ok $areOk count ${results.size} offsets ${
-                    cRecords.first().offset()
-                } to ${cRecords.last().offset()}\nKeys processed:\n${cRecords.map { it.key() }.joinToString("\n")}"
-            )
-            KafkaConsumerStates.IsFinished
         }
-    } catch (e: Exception) {
-        log.error { "Migration consume attempt fail: ${e.message}" }
+        val areOk = results.map { it.first }.filterIsInstance<KafkaConsumerStates.HasIssues>().isEmpty()
+        log.info {
+            "Migration check finished with ok flag $areOk count ${results.size} offsets ${
+                cRecords.first().offset()
+            } to ${cRecords.last().offset()}"
+        }
+        File("/tmp/migrationcheck").writeText(
+            "ok $areOk count ${results.size} offsets ${
+                cRecords.first().offset()
+            } to ${cRecords.last().offset()}\nKeys processed:\n${cRecords.map { it.key() }.joinToString("\n")}"
+        )
+        KafkaConsumerStates.IsFinished
+    }
+
+    AKafkaConsumer<String, String?>(
+        config = kafkaConsumerGcpMigration,
+        topics = listOf("pdl.geografisktilknytning-v1"),
+        fromBeginning = true
+    ).consume { cRecords ->
+        log.info { "Inside Migration Consumer GT" }
+        if (cRecords.isEmpty) {
+            if (workMetrics.recordsParsed.get().toInt() == 0 && retries > 0) {
+                log.info { "Migration GT : No records found $retries retries left, wait 60 w " }
+                retries--
+                Bootstrap.conditionalWait(60000)
+                return@consume KafkaConsumerStates.IsOk
+            } else {
+                log.info { "Migration GT : No more records found (or given up) - end consume session" }
+                return@consume KafkaConsumerStates.IsFinished
+            }
+        }
+
+        log.info { "Migration GT consumed batch of ${cRecords.count()}" }
+
+        log.info { "Migration GT first key ${cRecords.first().key()}" }
+
+        val results = cRecords.map { cr ->
+            if (cr.value() == null) {
+                Triple(KafkaConsumerStates.IsOk, null, cr.offset())
+            } else {
+                when (val gt = cr.value()!!.getGtFromJson()) {
+                    is Gt -> {
+                        Triple(KafkaConsumerStates.IsOk, gt, cr.offset())
+                    }
+                    else -> {
+                        Triple(KafkaConsumerStates.HasIssues, GtInvalid, cr.offset())
+                    }
+                }
+            }
+        }
+
+        val areOk = results.map { it.first }.filterIsInstance<KafkaConsumerStates.HasIssues>().isEmpty()
+        log.info {
+            "Migration GT check finished with ok flag $areOk count ${results.size} offsets ${
+                cRecords.first().offset()
+            } to ${cRecords.last().offset()}"
+        }
+        File("/tmp/migrationcheckGT").writeText(
+            "ok $areOk count ${results.size} offsets ${
+                cRecords.first().offset()
+            } to ${cRecords.last().offset()}\nKeys processed:\n${cRecords.map { it.key() }.joinToString("\n")}"
+        )
+        KafkaConsumerStates.IsFinished
     }
     log.info { "Migration check exit" }
 }
